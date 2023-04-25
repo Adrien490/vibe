43ddf4fa496490e44aa37a73bdaf1bccf6daba95
oui-connect-adrien <adrien.poirier@ynov.com>
2023-04-25 16:55:48 +0200

    test

--- src/components/shared/loader.tsx
+++ src/components/shared/loader.tsx
@@ -3,7 +3,7 @@ import { FaSpinner } from 'react-icons/fa';
 
 const Loader = () => {
   return (
-    <div className="fixed top-0 left-0 right-0 bottom-0 flex justify-center items-center">
+    <div className="z-20 fixed top-0 left-0 right-0 bottom-0 flex justify-center items-center">
       <FaSpinner size={25} className="animate-spin text-secondary" />
     </div>
   );
