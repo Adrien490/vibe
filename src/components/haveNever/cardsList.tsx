3043b65952a494ac716423fcd13e39e71b4ff896
oui-connect-adrien <adrien.poirier@ynov.com>
2023-04-30 16:29:53 +0200

    add title animation

--- src/components/haveNever/cardsList.tsx
+++ src/components/haveNever/cardsList.tsx
@@ -16,7 +16,7 @@ export const CardsList = ({shuffledData}:CardsListProps) => {
       className="absolute flex h-80 w-3/5 flex-col items-center justify-center gap-3 overflow-y-auto rounded-3xl border-2 border-white bg-primary p-3 shadow-2xl"
       style={{ zIndex: -index }}
     >
-      <h1 className="p-4 text-center text-xl text-white">Je n&apos;ai jamais</h1>
+      <h2 className="p-4 text-center text-xl text-white">Je n&apos;ai jamais</h2>
       <div className="break-words text-center italic text-white">
         {item.phrase}
       </div>
